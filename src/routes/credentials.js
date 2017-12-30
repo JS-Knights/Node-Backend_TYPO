a82b51b19ead1bad4f2e29b87e015cd3cda1bd3a
srsandy <sandeepranjan2007@yahoo.co.in>
2017-12-31 02:40:18 +0530

    make it better

--- src/routes/credentials.js
+++ src/routes/credentials.js
@@ -4,13 +4,13 @@ var router = express.Router();
 const createToken = require("../../utils/token.js");
 var passport = require("../../config/passport/passport.js");
 
-router.post('/signup', passport.authenticate("local-signup"), function(req, res) {
-  res.send({ token: createToken(req.user) });
-});
+// router.post('/signup', passport.authenticate("local-signup"), function(req, res) {
+//   res.send({ token: createToken(req.user) });
+// });
 
-router.post('/login', passport.authenticate("local-login"), function(req, res) {
-  res.send({ token: createToken(req.user) });
-});
+// router.post('/login', passport.authenticate("local-login"), function(req, res) {
+//   res.send({ token: createToken(req.user) });
+// });
 
 router.get('/', function(req, res) {
   res.send({x:'Welcome to API'});
